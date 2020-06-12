48269cb44ad7b25e940238dd005bdfd34a15d183
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-12 15:00:37 +0900

    基本動作

---
+++ app/models/task.rb
@@ -0,0 +1,10 @@
+class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_exclamation
+
+  private
+  def validate_name_not_including_exclamation
+    errors.add(:name, 'に！を加えることはできません') if name&.include?('!')
+  end
+end
