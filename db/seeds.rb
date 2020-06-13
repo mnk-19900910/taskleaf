2add5ff2e5130e7468fb22dde27a4126c4eb8f98
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-13 17:03:51 +0900

    taskとuserの関連修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@
 user = User.create(name: '山田哲人', email: "test@test.com", password_confirmation: "digest", created_at: "2020-06-12 15:20:00", updated_at: "2020-06-12 15:20:00")
 user2 = User.create(name: '青木宣親', email: "test2@test.com", password_confirmation: "digest", created_at: "2020-06-12 15:20:00", updated_at: "2020-06-12 15:20:00")
 user3 = User.create(name: '川端慎吾', email: "test3@test.com", password_confirmation: "digest", created_at: "2020-06-12 15:20:00", updated_at: "2020-06-12 15:20:00")
+user7 = User.create(id: '7',name: 'test', email: "test@test", password: 'test', password_confirmation: "test", created_at: "2020-06-12 15:20:00", updated_at: "2020-06-12 15:20:00")
