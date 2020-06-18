8b4596a1c8c124c2122064df0272e18a62997358
mnk-19900910 <wccf19900910@yahoo.co.jp>
2020-06-18 21:00:08 +0900

    terminal

--- app/javascript/taskleaf/hello.js
+++ app/javascript/taskleaf/hello.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     React.createElement('div', null, 'Hello World!'),
